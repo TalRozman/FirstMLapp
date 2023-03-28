9ecf36410e0be43067347f174607835c2b467441
TalRozman <talrozman9@outlook.com>
2023-03-28 16:47:15 +0300

    fixed connection error

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def learn():
     return render_template('learn.html',msg="learned")
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host="0.0.0.0",port=8000, debug=True)
